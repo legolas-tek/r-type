1e77f23e32616f19b0068fad9c061961192bd67e
BADER Marouan <marouan.bader@epitech.eu>
2023-09-22 22:46:48 +0200

    feat: add base components, position, velocity, drawable, controllable

---
+++ server/includes/drawable.hpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2023
+** r-type
+** File description:
+** drawable
+*/
+
+#ifndef DRAWABLE_HPP_
+#define DRAWABLE_HPP_
+
+struct drawable {
+    bool is_drawable;
+};
+
+#endif /* !DRAWABLE_HPP_ */
---
+++ server/includes/position.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2023
+** r-type
+** File description:
+** position
+*/
+
+#include <cstddef>
+
+#ifndef POSITION_HPP_
+#define POSITION_HPP_
+
+struct position {
+    size_t x;
+    size_t y;
+};
+
+#endif /* !POSITION_HPP_ */
