90ab2b9c101813894229f42cf97c5522e2e7e381
BADER Marouan <marouan.bader@epitech.eu>
2023-09-23 13:30:37 +0200

    feat: add an iterator that only iterates into initialised options

--- server/sources/systems.cpp
+++ server/sources/systems.cpp
@@ -14,8 +14,4 @@ void position_system(Registry &regi)
     for (const auto& i : regi.get_components<Component::position>()) {
         std::cout << "x = " << i->_x << " y = " << i->_y << std::endl;
     }
-    for (sparse_array<Component::position>::Components_iterator it = regi.get_components<Component::position>().begin(); it != regi.get_components<Component::position>().end(); ++it) {
-        std::cout << "x = " << *it->_x << " y = " << it->_y << std::endl;
-
-}
 }
