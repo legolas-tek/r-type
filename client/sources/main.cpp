c947a3ac5e10688caf115784a3de09269834b5b3
Jeremy Elalouf <jeremy.elalouf@epitech.eu>
2023-09-24 13:56:50 +0200

    feat: add debut of handling asio as a deps

--- client/sources/main.cpp
+++ client/sources/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
+#include "asio.hpp"
+#include "asio/ts/buffer.hpp"
+#include "asio/ts/internet.hpp"
+
 int main(int argc, char *argv[])
 {
+    asio::error_code ec;
+
+    asio::io_context context;
     std::cout << "Welcome to the R-Type client." << std::endl;
     return 0;
 }
