e00c9873702ed54a6c7602c3e2de6144d79b9175
Jeremy Elalouf <jeremy.elalouf@epitech.eu>
2023-10-03 00:54:19 +0200

    feat: move first ecs in his engine library

--- client/sources/main.cpp
+++ client/sources/main.cpp
@@ -4,26 +4,33 @@
 #include <asio/ts/buffer.hpp>
 #include <asio/ts/internet.hpp>
 
-#include <SFML/Graphics.hpp>
+#include "Registry.hpp"
+#include "Entity.hpp"
+#include "position.hpp"
+#include "velocity.hpp"
+
+#include "Logging_system.hpp"
 
 int main(int argc, char *argv[])
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
+    Registry reg;
+    Entity background(0);
+    Entity player(1);
+    Entity target(2);
 
-    while (window.isOpen())
-    {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
-        }
+    reg.register_component<Component::position>();
+    reg.register_component<Component::velocity>();
 
-        window.clear();
-        window.display();
-    }
+    reg.get_components<Component::position>().insert_at(player, std::move(Component::position(1, 200)));
+    reg.get_components<Component::position>().insert_at(target, std::move(Component::position(1, 500)));
+    reg.get_components<Component::velocity>().insert_at(player, std::move(Component::velocity(5, 0)));
+    reg.get_components<Component::velocity>().insert_at(target, std::move(Component::velocity(10, 0)));
+
+    reg.add_system<System::Logging>(reg.get_components<Component::position>(), reg.get_components<Component::velocity>());
+
+    reg.run_systems();
+    reg.run_systems();
+    reg.run_systems();
 
     return 0;
 }
