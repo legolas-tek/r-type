b09ac0dd54f7888d8c440f02300d49f72c9477af
Jeremy Elalouf <jeremy.elalouf@epitech.eu>
2023-10-02 18:27:51 +0200

    fix: binary are moved to a bin dir to link on windows

---
+++ cmake/windows-base.cmake
@@ -0,0 +1,14 @@
+#-------------------------------------------------------------------
+# This file is part of the CMake build system for R-Type
+# For the latest info, see https://github.com/legolas-tek/r-type
+#
+# The contents of this file are placed in the public domain. Feel
+# free to make use of it in any way you like.
+#-------------------------------------------------------------------
+
+set (CMAKE_SYSTEM_NAME Windows)
+set (CMAKE_SYSTEM_PROCESSOR amd)
+
+set (CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
+set (CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
+set (CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
