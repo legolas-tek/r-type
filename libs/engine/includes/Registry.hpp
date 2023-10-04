c64d6f58c0a15a6796e8f2ec421abea5f46278f2
Emil Pedersen <emil.pedersen@epitech.eu>
2023-10-04 17:39:20 +0200

    refactor(registry): use component ids as indices

--- libs/engine/includes/Registry.hpp
+++ libs/engine/includes/Registry.hpp
@@ -23,25 +23,31 @@ public:
     template <class Component> SparseArray<Component> &register_component()
     {
         SparseArray<Component> array;
-        auto emplace_result = _components_arrays.try_emplace(
-            typeid(SparseArray<Component>), std::move(array)
+        size_t component_id = _component_arrays.size();
+        auto emplace_result = _component_ids.try_emplace(
+            typeid(SparseArray<Component>), component_id
         );
-        if (emplace_result.second)
-            _erase_comp_funcs.emplace(
-                typeid(SparseArray<Component>),
-                [](Registry &registry, Entity const &entity) {
-                    registry.get_components<Component>().erase(entity);
-                }
-            );
+        if (!emplace_result.second)
+            return get_components<Component>(); // Already registered
+        _component_arrays.emplace_back(std::move(array));
+        _erase_component_funcs.emplace_back([](Registry &registry,
+                                               Entity const &entity) {
+            registry.get_components<Component>().erase(entity);
+        });
         return std::any_cast<SparseArray<Component> &>(
-            emplace_result.first->second
+            _component_arrays[component_id]
         );
+    }
+
+    template <class Component> size_t get_component_id() const
+    {
+        return _component_ids.at(typeid(SparseArray<Component>));
     }
 
     template <class Component> SparseArray<Component> &get_components()
     {
         return std::any_cast<SparseArray<Component> &>(
-            _components_arrays.at(typeid(SparseArray<Component>))
+            _component_arrays[get_component_id<Component>()]
         );
     }
 
@@ -49,13 +55,18 @@ public:
     SparseArray<Component> const &get_components() const
     {
         return std::any_cast<SparseArray<Component> const &>(
-            _components_arrays.at(typeid(SparseArray<Component>))
+            _component_arrays[get_component_id<Component>()]
         );
     }
 
     template <class Component> void erase_component(Entity const &entity)
     {
-        _erase_comp_funcs[typeid(SparseArray<Component>)](*this, entity);
+        _erase_component_funcs[get_component_id<Component>()](*this, entity);
+    }
+
+    void erase_component(Entity const &entity, size_t component_id)
+    {
+        _erase_component_funcs[component_id](*this, entity);
     }
 
     template <typename System, class... Params>
@@ -72,10 +83,19 @@ public:
     }
 
 private:
-    std::unordered_map<std::type_index, std::any> _components_arrays;
-    std::unordered_map<
-        std::type_index, std::function<void(Registry &, Entity const &)>>
-        _erase_comp_funcs;
+    /**
+     * Map of component type to component id
+     */
+    std::unordered_map<std::type_index, size_t> _component_ids;
+    /**
+     * 'Map' of component id to component array
+     */
+    std::vector<std::any> _component_arrays;
+    /**
+     * 'Map' of component id to erase function
+     */
+    std::vector<std::function<void(Registry &, Entity const &)>>
+        _erase_component_funcs;
     std::vector<std::unique_ptr<ISystem>> _systems;
 };
 
