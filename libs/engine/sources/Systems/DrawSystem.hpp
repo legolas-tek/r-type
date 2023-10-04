558f62ba20ff2e3037da86a7b294c01274181345
BADER Marouan <marouan.bader@epitech.eu>
2023-10-04 21:29:37 +0200

    chore: uselesse files, feat: add necessary files to start systems

---
+++ libs/engine/sources/Systems/DrawSystem.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2023
+** engine
+** File description:
+** DrawSystem
+*/
+
+#ifndef DRAWSYSTEM_HPP_
+#define DRAWSYSTEM_HPP_
+
+struct DrawSystem {
+        DrawSystem();
+        ~DrawSystem();
+
+};
+
+#endif /* !DRAWSYSTEM_HPP_ */
