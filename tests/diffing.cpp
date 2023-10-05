bfd43ea355e5d9b01184da9fdd99c29e6b7bc232
Emil Pedersen <emil.pedersen@epitech.eu>
2023-10-05 23:02:35 +0200

    build: add unit testing (#33)
    * build: add unit testing
    
    * style: merge link libraries

---
+++ tests/diffing.cpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2023
+** rtype tests
+** File description:
+** diffing
+*/
+
+#include <gtest/gtest.h>
+
+#include "Snapshot.hpp"
+
+TEST(Snapshot, diffing)
+{
+    EXPECT_EQ(1, 1);
+}
