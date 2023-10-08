c3355c134a6ecb4ce23360f1e59cc6d5a2bf3952
Emil Pedersen <emil.pedersen@epitech.eu>
2023-10-08 14:35:27 +0200

    Feature/networking/diffing (#36)
    * feat(networking): implement snapshot diffing
    
    * test: initial diffing
    
    * test: add test for removal
    
    * test: diffing updates
    
    * fix: compilation of unit tests
    
    * chore: remove useless include

--- libs/networking/sources/Snapshot.cpp
+++ libs/networking/sources/Snapshot.cpp
@@ -6,7 +6,7 @@
 */
 
 #include "Snapshot.hpp"
-#include "Registry.hpp"
+#include <cstring>
 #include <vector>
 
 Snapshot::Snapshot()
@@ -23,7 +23,86 @@ Snapshot::Snapshot(size_t tick, Registry const &registry)
 {
 }
 
+using EntityNumber = uint32_t;
+using ComponentId = uint8_t;
+using UpdateType = bool;
+
+static void diffAdd(
+    std::vector<std::byte> &diff,
+    std::vector<ComponentData>::const_iterator const &it
+)
+{
+    size_t offset = diff.size();
+    diff.resize(
+        offset + sizeof(EntityNumber) + sizeof(ComponentId) + sizeof(UpdateType)
+        + it->data.size()
+    );
+    std::memcpy(&diff[offset], &it->entity, sizeof(EntityNumber));
+    offset += sizeof(EntityNumber);
+    std::memcpy(&diff[offset], &it->componentId, sizeof(ComponentId));
+    offset += sizeof(ComponentId);
+    diff[offset] = std::byte(0x01);
+    offset += sizeof(UpdateType);
+    std::memcpy(&diff[offset], it->data.data(), it->data.size());
+}
+
+static void diffRemove(
+    std::vector<std::byte> &diff,
+    std::vector<ComponentData>::const_iterator const &it
+)
+{
+    size_t offset = diff.size();
+    diff.resize(
+        offset + sizeof(EntityNumber) + sizeof(ComponentId) + sizeof(UpdateType)
+    );
+    std::memcpy(&diff[offset], &it->entity, sizeof(EntityNumber));
+    offset += sizeof(EntityNumber);
+    std::memcpy(&diff[offset], &it->componentId, sizeof(ComponentId));
+    offset += sizeof(ComponentId);
+    diff[offset] = std::byte(0x00);
+}
+
 std::vector<std::byte>
 diffSnapshots(Snapshot const &previous, Snapshot const &current)
 {
+    std::vector<std::byte> diff;
+    auto previousIt = previous.data.begin();
+
+    for (auto currentIt = current.data.begin(); currentIt != current.data.end();
+         ++currentIt) {
+        if (previousIt == previous.data.end()) {
+            diffAdd(diff, currentIt);
+            continue;
+        }
+        if (previousIt->componentId == currentIt->componentId
+            && previousIt->entity == currentIt->entity) {
+            if (previousIt->data != currentIt->data) {
+                // modified, send again
+                diffAdd(diff, currentIt);
+            } // else, unchanged, skip
+            ++previousIt;
+            continue;
+        }
+        // different identifiers, either a removal or an addition
+        for (auto it = previousIt; it != previous.data.end(); ++it) {
+            if (it->componentId == currentIt->componentId
+                && it->entity == currentIt->entity) {
+                // current exists in previous, so there was a removal
+                // for each component in previous that is not in current
+                for (auto it2 = previousIt; it2 != it; ++it2) {
+                    diffRemove(diff, it2);
+                }
+                previousIt = it + 1;
+                --currentIt; // reprocess current
+                break;
+            }
+        }
+        // current does not exist in previous, so it was added
+        diffAdd(diff, currentIt);
+    }
+    // all remaining components in previous are removals
+    for (auto it = previousIt; it != previous.data.end(); ++it) {
+        diffRemove(diff, it);
+    }
+    return diff;
 }
--- tests/diffing.cpp
+++ tests/diffing.cpp
@@ -6,10 +6,119 @@
 */
 
 #include <gtest/gtest.h>
+#include <vector>
 
 #include "Snapshot.hpp"
 
-TEST(Snapshot, diffing)
+std::byte operator ""_b(unsigned long long value)
 {
-    EXPECT_EQ(1, 1);
+    return std::byte(value);
+}
+
+TEST(Diffing, Empty)
+{
+    Snapshot dummy;
+    Snapshot empty;
+
+    auto diff = diffSnapshots(dummy, empty);
+    ASSERT_EQ(diff.size(), 0);
+}
+
+TEST(Diffing, Initial)
+{
+    Snapshot dummy;
+    Snapshot current;
+
+    current.tick = 3;
+    current.data.push_back(ComponentData { .entity = Entity(0x01),
+                                           .componentId = 0x02,
+                                           .data = { 0x03_b, 0x04_b, 0x05_b } });
+    current.data.push_back(ComponentData { .entity = Entity(0x02),
+                                           .componentId = 0x02,
+                                           .data = { 0x04_b, 0x05_b, 0x06_b } });
+    std::vector<std::byte> diff = diffSnapshots(dummy, current);
+    std::vector<std::byte> expected = {
+        // First Component Data
+        0x01_b, 0x00_b, 0x00_b, 0x00_b, // entity number
+        0x02_b, // component id
+        0x01_b, // added
+        0x03_b, 0x04_b, 0x05_b, // data
+        // Second Component Data
+        0x02_b, 0x00_b, 0x00_b, 0x00_b, // entity number
+        0x02_b, // component id
+        0x01_b, // added
+        0x04_b, 0x05_b, 0x06_b, // data
+    };
+
+    ASSERT_EQ(diff, expected);
+}
+
+TEST(Diffing, RemoveOnly)
+{
+    Snapshot one;
+    Snapshot empty;
+
+    one.data.push_back(ComponentData { .entity = Entity(0x01),
+                                           .componentId = 0x02,
+                                           .data = { 0x03_b, 0x04_b, 0x05_b } });
+    std::vector<std::byte> diff = diffSnapshots(one, empty);
+    std::vector<std::byte> expected = {
+        // Component Data
+        0x01_b, 0x00_b, 0x00_b, 0x00_b, // entity number
+        0x02_b, // component id
+        0x00_b, // removed
+    };
+
+    ASSERT_EQ(diff, expected);
+}
+
+TEST(Diffing, SingleUpdate)
+{
+    Snapshot previous;
+    Snapshot current;
+
+    previous.data.push_back(ComponentData { .entity = Entity(0x01),
+                                           .componentId = 0x02,
+                                           .data = { 0x05_b, 0x04_b, 0x05_b } });
+    current.data.push_back(ComponentData { .entity = Entity(0x01),
+                                           .componentId = 0x02,
+                                           .data = { 0x03_b, 0x04_b, 0x05_b } });
+    std::vector<std::byte> diff = diffSnapshots(previous, current);
+    std::vector<std::byte> expected = {
+        // First Component Data
+        0x01_b, 0x00_b, 0x00_b, 0x00_b, // entity number
+        0x02_b, // component id
+        0x01_b, // updated
+        0x03_b, 0x04_b, 0x05_b, // data
+    };
+
+    ASSERT_EQ(diff, expected);
+}
+
+
+TEST(Diffing, RemoveAndAdd)
+{
+    Snapshot previous;
+    Snapshot current;
+
+    previous.data.push_back(ComponentData { .entity = Entity(0x01),
+                                           .componentId = 0x02,
+                                           .data = { 0x05_b } });
+    current.data.push_back(ComponentData { .entity = Entity(0x01),
+                                           .componentId = 0x03,
+                                           .data = { 0x03_b, 0x04_b, 0x05_b } });
+    std::vector<std::byte> diff = diffSnapshots(previous, current);
+    std::vector<std::byte> expected = {
+        // Second Component Data
+        0x01_b, 0x00_b, 0x00_b, 0x00_b, // entity number
+        0x03_b, // component id
+        0x01_b, // added
+        0x03_b, 0x04_b, 0x05_b, // data
+        // First Component Data
+        0x01_b, 0x00_b, 0x00_b, 0x00_b, // entity number
+        0x02_b, // component id
+        0x00_b, // removed
+    };
+
+    ASSERT_EQ(diff, expected);
 }
