511d1ccd42b81aca2c261fc1f0e56997b145eb57
Lindon Aliu <91671191+LindonAliu@users.noreply.github.com>
2023-10-08 20:13:44 +0200

    test on windows

--- client/sources/main.cpp
+++ client/sources/main.cpp
@@ -1,13 +1,14 @@
-#include <iostream>
-#include <ciso646>
 #include "UdpNetManager.hpp"
+#include <ciso646>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     net::manager::Udp ntm(net::client_netmanager, "127.0.0.1", 4242);
     net::manager::Udp::Buffer buff(10, std::byte('a'));
-    std::vector<std::pair<net::manager::Udp::Buffer, net::manager::Udp::Client>> res;
-    bool onetime = true;$
+    std::vector<std::pair<net::manager::Udp::Buffer, net::manager::Udp::Client>>
+        res;
+    bool onetime = true;
 
 
 
@@ -27,27 +28,16 @@ int main(int argc, char *argv[])
 
 
 
+                             
 
-
-
-
-
-
-
-
-
-
-
-
-                                        
-
-    while (1) {
+        while (1)
+    {
         try {
             if (onetime) {
                 ntm.send(buff);
                 onetime = false;
             }
-            if (not (res = ntm.receive()).empty()) {
+            if (not(res = ntm.receive()).empty()) {
                 std::cout << "Packet received" << std::endl;
                 break;
             }
