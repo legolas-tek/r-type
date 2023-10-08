49694bc78f7ac4d26e1391dec90f785ed380ad4d
Lindon Aliu <91671191+LindonAliu@users.noreply.github.com>
2023-10-08 20:11:46 +0200

    fix(pre-commit): if is directory not launch

--- client/sources/main.cpp
+++ client/sources/main.cpp
@@ -7,7 +7,39 @@ int main(int argc, char *argv[])
     net::manager::Udp ntm(net::client_netmanager, "127.0.0.1", 4242);
     net::manager::Udp::Buffer buff(10, std::byte('a'));
     std::vector<std::pair<net::manager::Udp::Buffer, net::manager::Udp::Client>> res;
-    bool onetime = true;
+    bool onetime = true;$
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                        
 
     while (1) {
         try {
