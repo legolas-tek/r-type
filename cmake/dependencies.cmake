98085d3722a993b7605dca53ccc146673890f74b
Yunse LEE <yunselee@gmail.com>
2023-10-08 16:34:58 +0200

    fix: raylib to share lib

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -56,7 +56,8 @@ FetchContent_Declare(raylib
     GIT_REPOSITORY "https://github.com/raysan5/raylib.git"
     GIT_TAG "master"
     GIT_PROGRESS TRUE
-
 )
+
+set(BUILD_SHARED_LIBS=ON)
 
 FetchContent_MakeAvailable(raylib)
