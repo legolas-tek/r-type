1bd48cd69019d2ee4adaa7143671d34a83727287
Yunse LEE <yunselee@gmail.com>
2023-10-10 12:08:10 +0200

    feat: set key wasd ans zqsd

--- libs/rendering/sources/Key.hpp
+++ libs/rendering/sources/Key.hpp
@@ -28,13 +28,25 @@ class Key : public ISystem {
             for (auto it = velocity_list.begin(); it != velocity_list.end(); ++it) {
                 bool isControllable = controlable_list[it.get_entity()].has_value();
 
+                // wasd et zqsd
+
                 // pos 있으면
                 if (isControllable) {
-                    if (IsKeyDown(KEY_W)) {
-                        std::cout << "entity " << it.get_entity() << "is pressed" << std::endl;
-                        velocity_list[it.get_entity()].value()._vx = 0.0f;
-                        velocity_list[it.get_entity()].value()._vy = -0.1f;
-
+                    if (IsKeyDown(KEY_W) || IsKeyDown(KEY_Z)) {
+                        velocity_list[it.get_entity()].value()._vx += 0.0f;
+                        velocity_list[it.get_entity()].value()._vy += -0.1f;
+                    }
+                    if (IsKeyDown(KEY_A) || IsKeyDown(KEY_Q)) {
+                        velocity_list[it.get_entity()].value()._vx += -0.1f;
+                        velocity_list[it.get_entity()].value()._vy += 0.0f;
+                    }
+                    if (IsKeyDown(KEY_S)) {
+                        velocity_list[it.get_entity()].value()._vx += 0.0f;
+                        velocity_list[it.get_entity()].value()._vy += 0.1f;
+                    }
+                    if (IsKeyDown(KEY_D)) {
+                        velocity_list[it.get_entity()].value()._vx += 0.1f;
+                        velocity_list[it.get_entity()].value()._vy += 0.0f;
                     }
                 }
             }
