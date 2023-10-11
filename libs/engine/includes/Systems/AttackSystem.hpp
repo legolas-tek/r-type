839410add258d805ac01f5ba73249dbf7791dfdf
BADER Marouan <marouan.bader@epitech.eu>
2023-10-11 22:10:58 +0200

    feat: add Attack system first logic

--- libs/engine/includes/Systems/AttackSystem.hpp
+++ libs/engine/includes/Systems/AttackSystem.hpp
@@ -10,8 +10,13 @@
 
 #include "SparseArray.hpp"
 #include "ISystem.hpp"
+#include "Entity.hpp"
+#include "Registry.hpp"
 
 #include "Components/Attack.hpp"
+#include "Components/HitBox.hpp"
+#include "Components/Position.hpp"
+#include "Components/Velocity.hpp"
 
 /// @brief This system checks wherever the entity is attacking with the attack
 /// component. If it does it launches an attack creating an entity and set back
@@ -20,7 +25,10 @@ namespace System {
 
 class AttackSystem : public ISystem {
 public:
-    AttackSystem(SparseArray<Component::Attack> &attacks) : _attacks(attacks) {}
+    AttackSystem(
+        SparseArray<Component::Attack> &attacks,
+        engine::Registry &reg
+        ) : _attacks(attacks), _register(reg) {}
 
     /// @brief deleted copy constructor
     AttackSystem(System::AttackSystem const &other) = delete;
@@ -28,10 +36,17 @@ public:
     ~AttackSystem() = default;
 
     void operator()() {
-        // func logic
+        for (auto &attack_comp : _attacks) {
+            if (attack_comp.is_attacking) {
+                engine::Entity attack(_register.get_new_entity());
+
+                // _register.get_components<Component::HitBox>().emplace_at(attack, )
+            }
+        }
     }
 private:
     SparseArray<Component::Attack> &_attacks;
+    engine::Registry &_register;
 };
 }
 
