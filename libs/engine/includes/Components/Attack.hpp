68eb529c7609eea09fe79e33f47b8d844201a247
BADER Marouan <marouan.bader@epitech.eu>
2023-10-11 09:57:03 +0200

    feat: add the attack component

---
+++ libs/engine/includes/Components/Attack.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2023
+** engine
+** File description:
+** Attack
+*/
+
+#ifndef ATTACK_HPP_
+#define ATTACK_HPP_
+
+#include "Serializable.hpp"
+
+/// @brief This struct is the component that represents the ability to attack
+/// of an entity
+namespace Component {
+
+struct Attack : Serializable<Attack> {
+    Attack() = default;
+    /// @brief A boolean that switch to true if and event triggers the attack
+    bool is_attacking = false;
+};
+}
+
+#endif /* !ATTACK_HPP_ */
