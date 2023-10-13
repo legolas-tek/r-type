18c5b29ac457e2f199f85dc0f40a3991ea20afd6
Yunse LEE <91876708+yunse-lee-EPITECH@users.noreply.github.com>
2023-10-14 01:06:25 +0200

    Feature/parallax player enemy (#69)
    * feat: basic parallax component and system
    
    * feat: basic parallax for a entitiy
    
    * feat: complete parallax system
    
    * style: delete all comments
    
    * style: delete unused print
    
    * feat: basic animation system
    
    * feat: add doxygen
    
    * style: clang format
    
    * feat: split ParallaxSystem and AnimationSystem with a cpp
    
    * chore: change = start_pos to += widh * scale for parallax
    
    * chore: clean and replace includes
    
    ---------
    
    Co-authored-by: Yunse LEE <yunselee@gmail.com>

--- libs/engine/includes/Components/Drawable.hpp
+++ libs/engine/includes/Components/Drawable.hpp
@@ -20,6 +20,7 @@ namespace Component {
  * component in a system for managing drawable entities.
  */
 struct Drawable : Serializable<Drawable> {
+public:
     /**
      * @brief Constructor for the Drawable structure.
      *
@@ -28,9 +29,16 @@ struct Drawable : Serializable<Drawable> {
      *
      * @param index The index to the resource associated with the drawable
      * entity.
+     * @param width The width of the drawable entity in pixels.
+     * @param height The height of the drawable entity in pixels.
+     * @param scale The scale factor to apply to the drawable entity (default
+     * is 1.0).
      */
-    Drawable(size_t index)
+    Drawable(size_t index, float width, float height, float scale = 1.0f)
         : _index(index)
+        , _width(width)
+        , _height(height)
+        , _scale(scale)
     {
     }
 
@@ -39,13 +47,13 @@ struct Drawable : Serializable<Drawable> {
      */
     Drawable() = default;
 
-    /**
-     * @brief The index to the resource associated in Registry::_assets_paths
-     * with the drawable entity.
-     */
-    size_t _index = 0;
+    size_t _index = 0; ///< The index to the associated resource in
+                       ///< Registry::_assets_paths.
+    float _width = 0.0f; ///< The width of the drawable entity in pixels.
+    float _height = 0.0f; ///< The height of the drawable entity in pixels.
+    float _scale = 1.0f; ///< The scale factor applied to the drawable entity.
 };
 
-}
+} // namespace Component
 
 #endif /* !Drawable_HPP_ */
