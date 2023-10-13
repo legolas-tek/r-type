0f222dd636001468b5259cee0dee82cf8073fd90
BADER Marouan <marouan.bader@epitech.eu>
2023-10-13 16:14:11 +0200

    feat: add AttackSystem class in AttackSystem.hpp

---
+++ libs/engine/includes/Systems/AttackSystem.hpp
@@ -0,0 +1,43 @@
+/*
+** EPITECH PROJECT, 2023
+** engine
+** File description:
+** AttackSystem
+*/
+
+#ifndef ATTACKSYSTEM_HPP_
+#define ATTACKSYSTEM_HPP_
+
+#include "SparseArray.hpp"
+#include "ISystem.hpp"
+#include "Entity.hpp"
+#include "Registry.hpp"
+
+#include "Components/Attack.hpp"
+
+/// @brief This system checks wherever the entity is attacking with the attack
+/// component. If it does it launches an attack creating an entity and set back
+/// the attack component to true.
+namespace System {
+
+class AttackSystem : public ISystem {
+public:
+    AttackSystem(
+        SparseArray<Component::Attack> &attacks,
+        engine::Registry &reg
+        );
+
+    /// @brief deleted copy constructor
+    AttackSystem(System::AttackSystem const &other) = delete;
+
+    ~AttackSystem() = default;
+
+    void operator()() override;
+private:
+    SparseArray<Component::Attack> &_attacks;
+    engine::Registry &_register;
+};
+}
+
+#endif /* !ATTACKSYSTEM_HPP_ */
+
