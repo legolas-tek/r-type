ea1e65f88fb897a8525a2f47dd17634d1dc7a007
BADER Marouan <marouan.bader@epitech.eu>
2023-10-16 01:38:52 +0200

    feature: add a different velocity and spawn point depending on if the attacking entity has the controllable component or not

--- libs/rtype/sources/Systems/AttackSystem.cpp
+++ libs/rtype/sources/Systems/AttackSystem.cpp
@@ -11,6 +11,7 @@
 #include "Components/Position.hpp"
 #include "Components/Velocity.hpp"
 #include "Components/Drawable.hpp"
+#include "Components/Controllable.hpp"
 
 System::AttackSystem::AttackSystem(
     SparseArray<Component::Attack> &attacks,
@@ -33,6 +34,7 @@ void System::AttackSystem::createLaserEntity(
     engine::Entity const attacker_index
     )
 {
+    Component::Velocity velocity(1, 0);
     SparseArray<Component::Position> &positions = _register.get_components<Component::Position>();
     engine::Entity attack_entity(_register.get_new_entity());
     Component::Position &attacker_pos = positions[attacker_index].value();
@@ -41,8 +43,12 @@ void System::AttackSystem::createLaserEntity(
         attacker_pos._y + (attack_comp.height / 2)
         );
 
+    if (_register.get_components<Component::Controllable>()[attacker_index]) {
+        velocity._vx = -1;
+        attack_entity_pos._x = attacker_pos._x - attack_comp.width - 1;
+    }
     positions.emplace_at(attack_entity, std::move(attack_entity_pos));
-    _register.get_components<Component::Velocity>().insert_at(attack_entity, Component::Velocity(1, 0));
+    _register.get_components<Component::Velocity>().insert_at(attack_entity, std::move(velocity));
     _register.get_components<Component::HitBox>().insert_at(attack_entity, Component::HitBox());
     _register.get_components<Component::Drawable>().insert_at(attack_entity, Component::Drawable(LASER_INDEX));
     attack_comp.is_attacking = false;
