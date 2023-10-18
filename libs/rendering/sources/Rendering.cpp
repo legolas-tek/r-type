1f5614a8d344d10d723dad7965fcbd3262b68e93
Lindon Aliu <91671191+LindonAliu@users.noreply.github.com>
2023-10-18 23:28:35 +0200

    WIP: refactor rendering for subsystem

--- libs/rendering/sources/Rendering.cpp
+++ libs/rendering/sources/Rendering.cpp
@@ -7,13 +7,20 @@
 */
 
 #include "Rendering.hpp"
+
 #include "Systems/Text.hpp"
+#include "Systems/Texture.hpp"
 
 rendering::system::Rendering::Rendering(engine::Registry &registry)
     : _registry(registry)
 {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib background scrolling");
 
+    AddSystem<rendering::system::Texture>(
+        registry, _registry.get_components<Component::Drawable>(),
+        _registry.get_components<Component::Animation>(),
+        _registry.get_components<Component::Position>()
+    );
     AddSystem<rendering::system::Text>(
         _registry.get_components<Component::Text>(),
         _registry.get_components<Component::Position>()
@@ -27,6 +34,8 @@ rendering::system::Rendering::~Rendering()
 
 void rendering::system::Rendering::operator()()
 {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
     /*auto drawable_list = _registry.get_components<Component::Drawable>();
     auto animation_list = _registry.get_components<Component::Animation>();
 
@@ -34,29 +43,27 @@ void rendering::system::Rendering::operator()()
         auto pos
             = _registry.get_components<Component::Position>()[it.get_entity()];
 
-    if (_cache.find(it.get_entity()) == _cache.end()) {
-        _cache.emplace(
-            it.get_entity(), _registry._assets_paths[(*it)->_index]
-        );
-    }
-    Texture2D texture = _cache.at(it.get_entity())._texture;
-    Rectangle sourceRec = { 0.0f, 0.0f, (*it)->_width, (*it)->_height };
-    if (animation_list[it.get_entity()].has_value()) {
-        auto rect_x
-            = animation_list[it.get_entity()].value()._current_offset;
-        sourceRec.x = rect_x;
-    }
-    float scale = (*it)->_scale;
-    Rectangle destRec = { pos->_x, pos->_y, sourceRec.width * scale,
-                          sourceRec.height * scale };
-    Vector2 origin = { 0, 0 };
-    float rotation = 0.0f;
-    Color tint = WHITE;
+        if (_cache.find(it.get_entity()) == _cache.end()) {
+            _cache.emplace(
+                it.get_entity(), _registry._assets_paths[(*it)->_index]
+            );
+        }
+        Texture2D texture = _cache.at(it.get_entity())._texture;
+        Rectangle sourceRec = { 0.0f, 0.0f, (*it)->_width, (*it)->_height };
+        if (animation_list[it.get_entity()].has_value()) {
+            auto rect_x
+                = animation_list[it.get_entity()].value()._current_offset;
+            sourceRec.x = rect_x;
+        }
+        float scale = (*it)->_scale;
+        Rectangle destRec = { pos->_x, pos->_y, sourceRec.width * scale,
+                              sourceRec.height * scale };
+        Vector2 origin = { 0, 0 };
+        float rotation = 0.0f;
+        Color tint = WHITE;
 
-    DrawTexturePro(texture, sourceRec, destRec, origin, rotation, tint);
-}*/
-    BeginDrawing();
-    ClearBackground(RAYWHITE);
+        DrawTexturePro(texture, sourceRec, destRec, origin, rotation, tint);
+    }*/
 
     for (auto &system : _subSystems) {
         (*system)();
---
+++ libs/rendering/sources/Systems/Texture.cpp
@@ -0,0 +1,58 @@
+/*
+** EPITECH PROJECT, 2023
+** R-Type
+** File description:
+** Texture System
+*/
+
+#include "Systems/Texture.hpp"
+
+rendering::system::Texture::Texture(
+    engine::Registry &registry, SparseArray<Component::Drawable> &drawables,
+    SparseArray<Component::Animation> &animations,
+    SparseArray<Component::Position> &positions
+)
+    : _registry(registry)
+    , _drawables(drawables)
+    , _animations(animations)
+    , _positions(positions)
+{
+}
+
+rendering::system::Texture::~Texture()
+{
+    for (auto &texture : _cache)
+        UnloadTexture(texture.second);
+}
+
+bool rendering::system::Texture::TextureIsLoaded(size_t id)
+{
+    return _cache.find(id) != _cache.end();
+}
+
+void rendering::system::Texture::operator()()
+{
+    auto &pos_list = _registry.get_components<Component::Position>();
+    for (auto it = _drawables.begin(); it != _drawables.end(); ++it) {
+        auto id = it.get_entity();
+        auto pos = pos_list[id];
+        auto anim = _animations[id];
+
+        std::cout << "pos[" << id << "]: " << pos->_x << ", " << pos->_y
+                  << std::endl;
+        if (not TextureIsLoaded(id))
+            _cache[id]
+                = LoadTexture(_registry._assets_paths[(*it)->_index].c_str());
+
+        Texture2D texture = _cache[id];
+        Rectangle sourceRec = { 0.0f, 0.0f, (*it)->_width, (*it)->_height };
+
+        if (anim.has_value())
+            sourceRec.x = (float) anim.value()._current_offset;
+
+        Rectangle destRec = { pos->_x, pos->_y, sourceRec.width * (*it)->_scale,
+                              sourceRec.height * (*it)->_scale };
+
+        DrawTexturePro(texture, sourceRec, destRec, { 0, 0 }, 0.0f, WHITE);
+    }
+}
