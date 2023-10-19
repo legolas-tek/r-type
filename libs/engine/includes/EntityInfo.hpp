c0de2ca31121ef780710a897625f56cc66e5d7e7
Yunse LEE <yunselee@gmail.com>
2023-10-19 17:45:41 +0200

    feat: add entityInfo struct

---
+++ libs/engine/includes/EntityInfo.hpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2023
+** r-type
+** File description:
+** EntityInfo
+*/
+
+#ifndef ENTITYINFO_HPP_
+#define ENTITYINFO_HPP_
+
+struct entityInfo {
+    int textureIndex;
+    int textureWidth;
+    int textureHeight;
+    int entityWidth;
+    int entityHeight;
+    int offset;
+    int frameDuration;
+    int posX;
+    int posY;
+    float scale;
+};
+#endif /* !ENTITYINFO_HPP_ */
