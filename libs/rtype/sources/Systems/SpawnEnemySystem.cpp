87aaa89a62ef9ded31f99e97a305edb4620c2db2
Yunse LEE <yunselee@gmail.com>
2023-10-20 02:02:15 +0200

    style: delete unused print

--- libs/rtype/sources/Systems/SpawnEnemySystem.cpp
+++ libs/rtype/sources/Systems/SpawnEnemySystem.cpp
@@ -19,43 +19,18 @@ System::SpawnEnemySystem::SpawnEnemySystem(engine::Registry &reg)
     _entityList.push_back(scourge);
 }
 
-// static void addEnemy1(engine::Registry &reg)
-// {
-//     engine::Entity enemy(reg.get_new_entity());
-//     float entityHeight = 27.0f;
-//     int randomY = std::rand()
-//         % static_cast<int>(rendering::system::SCREEN_HEIGHT - entityHeight);
-
-//     // set position
-//     reg.get_components<Component::Position>().insert_at(
-//         enemy, std::move(Component::Position(700, randomY, 0))
-//     );
-//     // set Drawable
-//     reg.get_components<Component::Drawable>().insert_at(
-//         enemy, std::move(Component::Drawable(6, 31.0f, 27.0f, 2.0f))
-//     );
-//     // set Animation
-//     reg.get_components<Component::Animation>().insert_at(
-//         enemy, std::move(Component::Animation(155, 27, 31, 27, 31, 10))
-//     );
-// }
-
 static void addEntity(engine::Registry &reg, EntityInfo entityInfo)
 {
-    std::cout << "111" << std::endl;
     engine::Entity enemy(reg.get_new_entity());
-    std::cout << "222" << std::endl;
     int randomY = std::rand()
         % static_cast<int>(
                       rendering::system::SCREEN_HEIGHT - entityInfo.entityHeight
         );
 
-    std::cout << "333" << std::endl;
     // set position
     reg.get_components<Component::Position>().insert_at(
         enemy, std::move(Component::Position(700, randomY, 0))
     );
-    std::cout << "444" << std::endl;
     // set Drawable
     reg.get_components<Component::Drawable>().insert_at(
         enemy,
@@ -64,7 +39,6 @@ static void addEntity(engine::Registry &reg, EntityInfo entityInfo)
             entityInfo.entityHeight, entityInfo.scale
         ))
     );
-    std::cout << "555" << std::endl;
     // set Animation
     reg.get_components<Component::Animation>().insert_at(
         enemy,
@@ -74,24 +48,13 @@ static void addEntity(engine::Registry &reg, EntityInfo entityInfo)
             entityInfo.frameDuration
         ))
     );
-    std::cout << "666" << std::endl;
 }
 
 void System::SpawnEnemySystem::addEnemy()
 {
-    std::cout << "aaa" << std::endl;
     int randomValue = (std::rand() % 2);
-    std::cout << "bbb " << randomValue << std::endl;
 
     addEntity(_register, _entityList[randomValue]);
-    // addEntity(reg, _entityList.);
-    // if (randomValue == 1) {
-    //     addEnemy1(reg);
-    // } else {
-    //     addEnemy2(reg);
-    // }
-    // for (auto &system : _subSystems)
-    //     (*system)();
 }
 
 void System::SpawnEnemySystem::operator()()
