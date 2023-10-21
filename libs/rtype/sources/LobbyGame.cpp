e6c729148d49ea081ced484f6e16f1d651196dd1
Emil Pedersen <emil.pedersen@epitech.eu>
2023-10-21 21:20:20 +0200

    feat(menu): add non working join button

--- libs/rtype/sources/LobbyGame.cpp
+++ libs/rtype/sources/LobbyGame.cpp
@@ -21,6 +21,8 @@
 
 #include "Rendering.hpp"
 
+static constexpr char const *FONT = "./assets/fonts/Over_There.ttf";
+
 void RTypeLobby::registerAllComponents(engine::Registry &reg)
 {
     reg.register_component<Component::Position>();
@@ -86,20 +88,33 @@ static engine::Entity createField(
 
     reg.get_components<Position>().emplace_at(labelEntity, Position(x, y));
     reg.get_components<Text>().emplace_at(
-        labelEntity,
-        Text(std::move(label), "./client/assets/fonts/Over_There.ttf", 20, 1)
+        labelEntity, Text(std::move(label), FONT, 20, 1)
     );
     reg.get_components<Position>().emplace_at(
         valueEntity, Position(x + 10, y + 30)
     );
     reg.get_components<Text>().emplace_at(
-        valueEntity,
-        Text(std::move(value), "./client/assets/fonts/Over_There.ttf", 20, 1)
+        valueEntity, Text(std::move(value), FONT, 20, 1)
     );
     reg.get_components<Focusable>().emplace_at(valueEntity, Focusable());
     reg.get_components<Editable>().emplace_at(valueEntity, Editable());
     reg.get_components<HitBox>().emplace_at(valueEntity, HitBox(300, 50));
     return valueEntity;
+}
+
+static engine::Entity
+createButton(engine::Registry &reg, std::string &&label, int x, int y)
+{
+    using namespace Component;
+    engine::Entity button(reg.get_new_entity());
+
+    reg.get_components<Position>().emplace_at(button, Position(x, y));
+    reg.get_components<Text>().emplace_at(
+        button, Text(std::move(label), FONT, 40, 1)
+    );
+    reg.get_components<Focusable>().emplace_at(button, Focusable());
+    reg.get_components<HitBox>().emplace_at(button, HitBox(100, 50));
+    return button;
 }
 
 void RTypeLobby::initScene(engine::Registry &reg)
@@ -108,11 +123,10 @@ void RTypeLobby::initScene(engine::Registry &reg)
     engine::Entity title(reg.get_new_entity());
 
     reg.get_components<Position>().emplace_at(title, Position(0, 0));
-    reg.get_components<Text>().insert_at(
-        title, Text("R-Type", "./client/assets/fonts/Over_There.ttf", 50, 10)
-    );
+    reg.get_components<Text>().insert_at(title, Text("R-Type", FONT, 50, 10));
 
     engine::Entity address = createField(reg, "Address", "127.0.0.1", 100, 100);
     engine::Entity port = createField(reg, "Port", "4242", 100, 200);
     engine::Entity name = createField(reg, "Name", "Player", 100, 300);
+    engine::Entity join = createButton(reg, "Join", 500, 200);
 }
