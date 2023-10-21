8b2e58a8e7dc780785c8b13b6540cf616741fedd
Lindon Aliu <91671191+LindonAliu@users.noreply.github.com>
2023-10-21 19:36:59 +0200

    feat: add editable component and system (#104)

--- libs/rtype/sources/LobbyGame.cpp
+++ libs/rtype/sources/LobbyGame.cpp
@@ -11,8 +11,11 @@
 #include "Components/Focusable.hpp"
 #include "Components/HitBox.hpp"
 #include "Components/Position.hpp"
+#include "Components/Text.hpp"
 
+#include "Systems/Editable.hpp"
 #include "Systems/Focusable.hpp"
+#include "Systems/Text.hpp"
 
 #include "Rendering.hpp"
 
@@ -22,6 +25,8 @@ void RTypeLobby::registerAllComponents(engine::Registry &reg)
     reg.register_component<Component::Drawable>();
     reg.register_component<Component::Focusable>();
     reg.register_component<Component::HitBox>();
+    reg.register_component<Component::Text>();
+    reg.register_component<Component::Editable>();
 }
 
 void RTypeLobby::registerAdditionalClientSystems(engine::Registry &reg)
@@ -32,6 +37,11 @@ void RTypeLobby::registerAdditionalClientSystems(engine::Registry &reg)
         reg.get_components<Component::HitBox>(),
         reg.get_components<Component::Position>()
     );
+    reg.add_system<rendering::system::Editable>(
+        reg.get_components<Component::Editable>(),
+        reg.get_components<Component::Focusable>(),
+        reg.get_components<Component::Text>()
+    );
 }
 
 void RTypeLobby::registerAdditionalServerSystems(engine::Registry &reg)
@@ -39,4 +49,16 @@ void RTypeLobby::registerAdditionalServerSystems(engine::Registry &reg)
     _serverLobby = dynamic_cast<net::lobby::Lobby &>(
         reg.add_system<net::lobby::Lobby>(4242, 4)
     );
+}
+
+void RTypeLobby::registerAdditionalSystems(engine::Registry &reg)
+{
+}
+
+void RTypeLobby::initAssets(engine::Registry &reg)
+{
+}
+
+void RTypeLobby::initScene(engine::Registry &reg)
+{
 }
