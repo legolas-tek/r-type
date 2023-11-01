bb70b51bdf2262b00ea88a2db83af1ea2dab47fc
Yunse LEE <yunselee@gmail.com>
2023-11-02 00:49:42 +0100

    fix: player in the correct pos

--- libs/rendering/sources/Key.cpp
+++ libs/rendering/sources/Key.cpp
@@ -26,13 +26,13 @@ void rendering::system::Key::operator()()
 {
     Component::Velocity velocity;
     if (IsKeyDown(KEY_W) || IsKeyDown(KEY_Z)) {
-        velocity._vy += 5.0f;
+        velocity._vy -= 5.0f;
     }
     if (IsKeyDown(KEY_A) || IsKeyDown(KEY_Q)) {
         velocity._vx -= 5.0f;
     }
     if (IsKeyDown(KEY_S)) {
-        velocity._vy -= 5.0f;
+        velocity._vy += 5.0f;
     }
     if (IsKeyDown(KEY_D)) {
         velocity._vx += 5.0f;
