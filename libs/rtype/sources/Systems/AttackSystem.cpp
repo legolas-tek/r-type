23e0c6e4aec45e2568f4dc49b18bd86370e5ea4f
BADER Marouan <marouan.bader@epitech.eu>
2023-11-03 03:00:51 +0100

    chore: change all useless insert_at into emplace_at

--- libs/rtype/sources/Systems/AttackSystem.cpp
+++ libs/rtype/sources/Systems/AttackSystem.cpp
@@ -60,32 +60,27 @@ void System::AttackSystem::createLaserEntity(engine::Entity const attacker_index
         attack_entity_pos._x = attacker_pos._x
             - (attacker_collision->_width / 2) - (LASER_WIDTH);
     }
-    _positions.insert_at(attack_entity, attack_entity_pos);
-    _register.get_components<Component::Velocity>().insert_at(
-        attack_entity, velocity
+    _positions.emplace_at(attack_entity, attack_entity_pos);
+    _register.get_components<Component::Velocity>().insert_at(attack_entity, velocity
     );
-    _register.get_components<Component::HitBox>().insert_at(
-        attack_entity, Component::HitBox(LASER_WIDTH, LASER_HEIGHT)
+    _register.get_components<Component::HitBox>().emplace_at(
+        attack_entity, LASER_WIDTH, LASER_HEIGHT
     );
-    _register.get_components<Component::Collision>().insert_at(
-        attack_entity, Component::Collision(LASER_WIDTH, LASER_HEIGHT)
+    _register.get_components<Component::Collision>().emplace_at(
+        attack_entity, LASER_WIDTH, LASER_HEIGHT
     );
-    _register.get_components<Component::Animation>().insert_at(
-        attack_entity,
-        Component::Animation(
-            LASER_WIDTH * LASER_FRAMES, LASER_HEIGHT, LASER_WIDTH, LASER_HEIGHT,
-            LASER_WIDTH, 10
-        )
+    _register.get_components<Component::Animation>().emplace_at(
+        attack_entity, LASER_WIDTH * LASER_FRAMES, LASER_HEIGHT, LASER_WIDTH,
+        LASER_HEIGHT, LASER_WIDTH, 10
     );
-    _register.get_components<Component::Drawable>().insert_at(
-        attack_entity,
-        Component::Drawable(LASER_INDEX, LASER_WIDTH, LASER_HEIGHT)
+    _register.get_components<Component::Drawable>().emplace_at(
+        attack_entity, LASER_INDEX, LASER_WIDTH, LASER_HEIGHT
     );
-    _register.get_components<Component::LifeTime>().insert_at(
-        attack_entity, Component::LifeTime(200, _register.getTick())
+    _register.get_components<Component::LifeTime>().emplace_at(
+        attack_entity, 200, _register.getTick()
     );
-    _register.get_components<Component::Damage>().insert_at(
-        attack_entity, Component::Damage(1)
+    _register.get_components<Component::Damage>().emplace_at(
+        attack_entity, 1
     );
 }
 
