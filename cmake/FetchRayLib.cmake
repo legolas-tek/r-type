99d9ff29243d098e31fcfd904799eaf4971bf468
Jeremy Elalouf <jeremy.elalouf@epitech.eu>
2023-11-05 04:24:30 +0100

    chore: cmake dependencies are fetched in named files

---
+++ cmake/FetchRayLib.cmake
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------
+# This file is part of the CMake build system for R-Type
+# For the latest info, see https://github.com/legolas-tek/r-type
+#
+# The contents of this file are placed in the public domain. Feel
+# free to make use of it in any way you like.
+#-------------------------------------------------------------------
+
+FetchContent_Declare (raylib
+    GIT_REPOSITORY "https://github.com/raysan5/raylib.git"
+    GIT_TAG "master"
+    GIT_PROGRESS TRUE
+)
+
+set (CUSTOMIZE_BUILD ON)
+set (BUILD_SHARED_LIBS ON CACHE INTERNAL "")
+
+FetchContent_MakeAvailable (raylib)
